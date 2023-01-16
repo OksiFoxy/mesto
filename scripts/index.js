cbf78064a04141193a6e773163d87ac164d072f1
Oksana Shishova <oksi-love@yandex.ru>
2023-01-16 23:42:08 +0300

    refactor: исправлены ошибки после проверки

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,20 @@ let nameForm = document.querySelector ('.profile__title');
 let jobForm = document.querySelector ('.profile__subtitle');
 let popupProfile = document.querySelector ('.popup');
 let popupProfileOpen = document.querySelector ('.profile__button-edit');
-let popupProfileForm = document.querySelector ('.popup__form');
-let nameInput = document.querySelector ('.popup__input-name');
-let jobInput = document.querySelector ('.popup__input-job');
+let popupProfileForm = document.querySelector ('.popup_form');
+let nameInput = document.querySelector ('.popup__input_name');
+let jobInput = document.querySelector ('.popup__input_job');
 let popupProfileSave = document.querySelector ('.popup__submit-button');
 let popupProfileClose = document.querySelector ('.popup__close-form');
 
 // // Открытие формы профайла
-popupProfileOpen.addEventListener('click', function openProfile () {
+function openProfile () {
   popupProfile.classList.add('popup_opened');
-});
+  nameForm.textContent = nameInput.value;
+  jobForm.textContent = jobInput.value;
+};
+
+popupProfileOpen.addEventListener('click', openProfile);
 
 // Закрытие формы профайла
 function closeProfile () {
@@ -22,10 +26,12 @@ function closeProfile () {
 popupProfileClose.addEventListener ('click', closeProfile);
 
 // Отправка формы
-popupProfile.addEventListener('submit', function handleFormSubmit (evt) {
+function handleFormSubmit (evt) {
   evt.preventDefault();
   nameForm.textContent = nameInput.value;
   jobForm.textContent = jobInput.value;
   console.log('Изменения сохранены');
   closeProfile ();
-});
+}
+
+popupProfile.addEventListener('submit', handleFormSubmit);
