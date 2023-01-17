0ac5e7f25cad8003ccfa8936d72e4a188f3488dc
Oksana Shishova <oksi-love@yandex.ru>
2023-01-17 15:14:01 +0300

    refactor: Исправления после 2й проверки

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,19 @@
 // Форма редактирования профиля
-let nameForm = document.querySelector ('.profile__title');
-let jobForm = document.querySelector ('.profile__subtitle');
+let nameForm = document.querySelector ('.profile__name');
+let jobForm = document.querySelector ('.profile__about');
 let popupProfile = document.querySelector ('.popup');
 let popupProfileOpen = document.querySelector ('.profile__button-edit');
 let popupProfileForm = document.querySelector ('.popup_form');
 let nameInput = document.querySelector ('.popup__input_name');
 let jobInput = document.querySelector ('.popup__input_job');
 let popupProfileSave = document.querySelector ('.popup__submit-button');
-let popupProfileClose = document.querySelector ('.popup__close-form');
+let popupProfileClose = document.querySelector ('.popup__close');
 
 // // Открытие формы профайла
 function openProfile () {
   popupProfile.classList.add('popup_opened');
-  nameForm.textContent = nameInput.value;
-  jobForm.textContent = jobInput.value;
+  nameInput.value = nameForm.textContent;
+  jobInput.value = jobForm.textContent;
 };
 
 popupProfileOpen.addEventListener('click', openProfile);
